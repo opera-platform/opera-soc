9834eae50de7e212fef96c811ea80bcb5598a73e
pznikola <p.z.nikola@etf.rs>
2026-07-12 14:29:33 +0200

    added configs for dsp chain testing

--- generators/chipyard/src/main/scala/config/OperaDspConfigs.scala
+++ generators/chipyard/src/main/scala/config/OperaDspConfigs.scala
@@ -3,8 +3,19 @@ package chipyard
 import chipyard.example.operadsp.{OperaDspChainParams, WithOperaDspChain}
 import org.chipsalliance.cde.config.Config
 
+// DSP chain with a single Rocket core, used for testing the DSP chain in isolation
 class OperaDspRocketConfig extends Config(
-  new WithOperaDspChain(OperaDspChainParams(numPoints = 16)) ++
+  new WithOperaDspChain(OperaDspChainParams(numPoints = 256)) ++
+  new freechips.rocketchip.rocket.WithNHugeCores(1) ++
+  new chipyard.config.AbstractConfig
+)
+
+// DSP chain with Ethernet loopback and a single Rocket core, used for testing the DSP chain with network connectivity
+class OperaDspEthLoopbackRocketConfig extends Config(
+  new chipyard.harness.WithEthernetRGMIILoopback ++
+  new rivet.wrapper.WithEthernetRGMIISim ++
+  new WithOperaDspChain(OperaDspChainParams(numPoints = 256)) ++
+  new freechips.rocketchip.rocket.WithCFlushEnabled ++
   new freechips.rocketchip.rocket.WithNHugeCores(1) ++
   new chipyard.config.AbstractConfig
 )
